672bd2adcbfa600f0bb29b8c802da23616d99a2f
Fedor <feda.vert@gmail.com>
2025-04-03 10:16:03 +0300

    Вариант 6 ЕГЭ (Агето). Набрано 22/29 баллов (75.86%)

--- 24.py
+++ 24.py
@@ -1,14 +1,14 @@
 s = open('24_15339.txt').readline()
 
-s = s.replace('B', 'A').replace('C', 'A')
+s = s.replace('B', 'A').replace('C', 'A') # Заменяем все буквы на букву "А"
 
 for x in '789':
-    s = s.replace(x, '6')
+    s = s.replace(x, '6')  # Заменяем все цифры на цифру "6"
 
-while 'AA' in s or '66' in s:
+while 'AA' in s or '66' in s: # ока нашлись нарушения в строке
     if 'AA' in s:
-        s = s.replace('AA', 'A A')
+        s = s.replace('AA', 'A A') # Никакая буква не стоит рядом с буквой
     if '66' in s:
-        s = s.replace('66', '6 6')
+        s = s.replace('66', '6 6') # Никакая цифра не стоит рядом с цифрой
 
 print( max(len(c) for c in s.split() ) )
